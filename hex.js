b4b51305fd2861a01b983a0ee0cef344f3a12521
Youri <youri.kassianoff@gmail.com>
2022-10-19 10:09:51 +0200

    project color flipper

--- hex.js
+++ hex.js
@@ -1,6 +1,8 @@
-const generateColor = () => {
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function () {
   let randomColor = Math.floor(Math.random() * 0xffffff).toString(16);
-  document.body.style.backgroundColor = "#" + randomColor;
+  document.body.style.background = "#" + randomColor;
   let text = document.querySelector("#color");
-  text.innerText = "#" + randomColor;
-};
+  text.innerText = randomColor;
+});
